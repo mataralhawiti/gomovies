a48096af47afcc2e6d59b6392b412bef03afd76e
Matar7 <matar@linux.com>
2023-06-19 12:18:44 +0300

    break down bigQuery code

---
+++ bigquery/bigqueryops.go
@@ -0,0 +1,19 @@
+package bigquery
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/bigquery"
+)
+
+func ImportParquet(projectID, datasetID, tableID string) error {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		return fmt.Errorf("bigquery.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	return nil
+}
