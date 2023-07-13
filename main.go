8f3e0638e2030c0f8fe83200ac496b56d5d39b39
Matar7 <matar@linux.com>
2023-07-13 14:02:06 +0300

    finished most of Bigquery code

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/mataralhawiti/gomovies/bigquery"
 	"github.com/mataralhawiti/gomovies/internal" // the directory Not the file!
 	"github.com/mataralhawiti/gomovies/parser"   // the directory Not the file!
+	"google.golang.org/api/iterator"
+
+	bq "cloud.google.com/go/bigquery"
 )
 
 func main() {
@@ -21,8 +26,8 @@ func main() {
 
 	// bigquery
 	projectID := "golang-389808"
-	dataSet := "dummyds"
-	table := "dumtable"
+	// dataSet := "dummyds"
+	// table := "dumtable"
 	c := bigquery.CreateBqClient(projectID)
 	//2023/06/13 14:59:34 bigquery.NewClient: bigquery: constructing client: google: could not find default credentials.
 	//See https://cloud.google.com/docs/authentication/external/set-up-adc for more information
@@ -34,8 +39,34 @@ func main() {
 	//bigquery.CreateTable(dataSet, table, c)
 
 	// insert into bq table
-	bigquery.InsertIntoBq(dataSet, table, c, *myMovies)
+	// bigquery.InsertIntoBq(dataSet, table, c, *myMovies)
 
 	// query bq table
-	// ReadBG := bigquery.ReadFromBq()
+	//sqlText := "SELECT movie_id, movie_url, name, year, rating, `desc` FROM dummyds.dumtable LIMIT 2"
+	//sqlText := "SELECT count(*) from dummyds.dumtable"
+	sqlText := "SELECT count(*) from dummyds.dumtable WHERE year = ?;"
+
+	q := c.Query(sqlText)
+	// params_positional
+	q.Parameters = []bq.QueryParameter{
+		{Value: "2012"},
+	}
+
+	ctx := context.Background()
+	it, err := q.Read(ctx)
+	if err != nil {
+		fmt.Println("Something is wrong")
+	}
+
+	for {
+		var values []bq.Value //interface
+		err := it.Next(&values)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println("handle the error")
+		}
+		fmt.Println(values)
+	}
 }
