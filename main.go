c5c103c086fe6387de2c897b354a563bcd8091be
Matar7 <matar@linux.com>
2023-08-10 15:53:31 +0300

    enhance the implementation

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"os"
 
 	"github.com/mataralhawiti/gomovies/internal" // the directory Not the file!
 	"github.com/mataralhawiti/gomovies/parser"   // the directory Not the file!
 )
 
+const filePath = "async_movies_full.json"
+
 var (
 	// Flags
 	runMod        = flag.Int("mod", 0, "0 to display stats about your provided movies json or 1 to run python scrtip to generate movies json file. Default: 0")
-	filePath      = flag.String("fp", ".", "movies JSON file path")
-	displayStats  = flag.Bool("st", true, "Display stats about my movies. Default: true")
 	PythonBinPath = flag.String("py", "python", "Your local machine python bin path. Default python")
 )
 
@@ -19,17 +22,29 @@ func main() {
 	// parse all flags
 	flag.Parse()
 
-	if *runMod == 1 {
-		// exect python script
+	switch *runMod {
+	case 0:
+		// parse movies json file & display stats about your movies list
+		myMovies := parser.ParseJSON(filePath)
+		internal.DisplayInfo(myMovies)
+	case 1:
 		parser.RunPyScript(*PythonBinPath)
 		return
+	case 3:
+		doesFileExist(filePath)
+	default:
+		log.Fatal("invalid runMod")
 	}
+}
 
-	// parse movies json file
-	myMovies := parser.ParseJSON(*filePath)
+// function to check if file exists
+func doesFileExist(fileName string) {
+	_, error := os.Stat(fileName)
 
-	// New imple - display stats about your movies list
-	if *displayStats {
-		internal.DisplayInfo(myMovies)
+	// check if error is "file not exists"
+	if os.IsNotExist(error) {
+		fmt.Printf("%v file does not exist\n", fileName)
+	} else {
+		fmt.Printf("%v file exist\n", fileName)
 	}
 }
