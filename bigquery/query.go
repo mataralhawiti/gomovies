9723d1954bba86a51b3e500611c432d10cd77220
Matar7 <matar@linux.com>
2023-08-20 13:38:29 +0300

    test bigquery read()

--- bigquery/query.go
+++ bigquery/query.go
@@ -1,34 +1,39 @@
 package bigquery
 
+import (
+	"strconv"
+)
+
 var QList = map[string]string{
-	"Count":             "SELECT count(movie_id) FROM movies.movies_full;",
-	"CountPerYear":      "SELECT count(*) FROM movies.movies_full WHERE year = ?;",
-	"CountGroupPerYear": "SELECT year, count(movie_id) as ctn FROM movies.movies_full GROUP BY year ORDER BY count(movie_id) DESC;",
-	"display1":          "SELECT movie_id, movie_url, name, year, rating, `desc` FROM movies.movies_full LIMIT 1",
+	"Movies count":                 "SELECT count(movie_id) FROM movies.movies_full;",
+	"Movies count per year":        "SELECT count(*) FROM movies.movies_full WHERE year = ?;",
+	"Moives count grouped by year": "SELECT year, count(movie_id) as ctn FROM movies.movies_full GROUP BY year ORDER BY count(movie_id) DESC;",
+	"display a movie info":         "SELECT name, year, rating, `desc` FROM movies.movies_full LIMIT 1",
 }
 
 type Query struct {
-	id      int
+	id      string
 	desc    string
 	sqlText string
 }
 
-func QueriesList() []map[string]string{
+func QueriesList() []map[string]string {
 	var q Query
 	//QMap := map[string]string{}
 	QSlice := []map[string]string{}
-	ctn := 0
+	ctn := 1
 
 	for k, v := range QList {
-		// q.id = ctn
-		// q.desc = k
-		// q.sqlText = v
+		q.id = strconv.Itoa(ctn)
+		q.desc = k
+		q.sqlText = v
+		QMap := map[string]string{"id": q.id, "desc": q.desc, "sqlText": q.sqlText}
+		QSlice = append(QSlice, QMap)
 
-		QSlice = append(QSlice, map{"id":cnt, k:v})
-		// fmt.Printf("%+v\n", q)
+		// clear QMap - running Go 1.20
+		QMap = map[string]string{}
 
-		//QMap[ctn] = q
-
+		ctn++
 	}
 
 	return QSlice
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +11,9 @@ import (
 	"github.com/mataralhawiti/gomovies/bigquery" // the directory Not the file!
 	"github.com/mataralhawiti/gomovies/internal" // the directory Not the file!
 	"github.com/mataralhawiti/gomovies/parser"   // the directory Not the file!
+
+	bq "cloud.google.com/go/bigquery"
+	"google.golang.org/api/iterator"
 )
 
 const filePath = "async_movies_full.json"
@@ -37,8 +42,59 @@ func main() {
 	case 4:
 		// BigQuery
 		ql := bigquery.QueriesList()
+		var ids []string
+
+		// get QueriesList ids
 		for _, k := range ql {
-			fmt.Printf("%+v", k)
+			ids = append(ids, k["id"])
+		}
+
+		// Take user input
+		fmt.Println("Please enter query id .. ex: 2")
+		for _, items := range ql {
+			fmt.Printf("%v - %v\n", items["id"], items["desc"])
+		}
+
+		var qur string
+		fmt.Scan(&qur)
+
+		var sqltxt string
+
+		// check if user's input is valid
+		// if yes, extract SQL query text from predefined list
+		if ok, err := contains(ids, qur); ok {
+			for _, k := range ql {
+				if sqltxt = k["sqlText"]; k["id"] == qur {
+					break
+				}
+			}
+		} else {
+			log.Fatal(err)
+		}
+
+		fmt.Println(sqltxt)
+
+		// BigQuery
+		projectID := "golang-389808"
+		c := bigquery.CreateBqClient(projectID)
+		q := c.Query(sqltxt)
+		ctx := context.Background()
+		it, err := q.Read(ctx)
+		if err != nil {
+			fmt.Println("Something is wrong")
+		}
+
+		for {
+			var values []bq.Value //interface
+			err := it.Next(&values)
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				fmt.Println("iterator error")
+			}
+			//fmt.Println(values)
+			fmt.Printf("%+v\n", values)
 		}
 
 	default:
@@ -56,4 +112,15 @@ func doesFileExist(fileName string) {
 	} else {
 		fmt.Printf("%v file exist\n", fileName)
 	}
+}
+
+// check if value in Slice - Go 1.20
+// For Go 1.21, we should use package:slices
+func contains(slic []string, v string) (bool, error) {
+	for _, s := range slic {
+		if s == v {
+			return true, nil
+		}
+	}
+	return false, errors.New("Invalid query number")
 }
