42325b7767c957bfce89067bce343895d5be11a8
Matar7 <matar@linux.com>
2023-08-21 12:14:27 +0300

    rename bigquery package

--- bigquery/base.go
+++ bigquery/base.go
@@ -5,18 +5,18 @@ import (
 	"fmt"
 	"log"
 
-	"cloud.google.com/go/bigquery"
+	bq "cloud.google.com/go/bigquery"
 	"github.com/mataralhawiti/gomovies/parser" // the directory Not the file!
 	"google.golang.org/api/iterator"
 )
 
 const BigQuery string = "BigQuery"
 
-func CreateBqClient(projectID string) *bigquery.Client {
+func CreateBqClient(projectID string) *bq.Client {
 	ctx := context.Background()
 
 	// Creates a client.
-	client, err := bigquery.NewClient(ctx, projectID)
+	client, err := bq.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatalf("bigquery.NewClient: %v", err)
 	}
@@ -25,28 +25,28 @@ func CreateBqClient(projectID string) *bigquery.Client {
 	return client
 }
 
-func CreateDataSet(datasetName string, bqClinet *bigquery.Client) {
+func CreateDataSet(datasetName string, bqClinet *bq.Client) {
 	ctx := context.Background()
-	if err := bqClinet.Dataset(datasetName).Create(ctx, &bigquery.DatasetMetadata{}); err != nil {
+	if err := bqClinet.Dataset(datasetName).Create(ctx, &bq.DatasetMetadata{}); err != nil {
 		log.Fatalf("Failed to create dataset: %v", err)
 	}
 	fmt.Printf("Dataset created\n")
 }
 
-func CreateTable(datasetName string, tableName string, bqClinet *bigquery.Client) {
-	schema, err := bigquery.InferSchema(parser.Movie{})
+func CreateTable(datasetName string, tableName string, bqClinet *bq.Client) {
+	schema, err := bq.InferSchema(parser.Movie{})
 	if err != nil {
 		log.Fatalf("Failed to create dataset: %v", err)
 	}
 	//schema, err := bqClinet.Dataset(datasetName).Table("ss").
 
 	ctx := context.Background()
-	if err := bqClinet.Dataset(datasetName).Table(tableName).Create(ctx, &bigquery.TableMetadata{Schema: schema}); err != nil {
+	if err := bqClinet.Dataset(datasetName).Table(tableName).Create(ctx, &bq.TableMetadata{Schema: schema}); err != nil {
 		log.Fatalf("Failed to create table: %v", err)
 	}
 }
 
-func InsertIntoBq(datasetName string, tableName string, bqClinet *bigquery.Client, mvs []parser.Movie) error {
+func InsertIntoBq(datasetName string, tableName string, bqClinet *bq.Client, mvs []parser.Movie) error {
 	ins := bqClinet.Dataset(datasetName).Table(tableName).Inserter()
 
 	ctx := context.Background()
@@ -57,7 +57,7 @@ func InsertIntoBq(datasetName string, tableName string, bqClinet *bigquery.Clien
 	return nil
 }
 
-func ReadFromBq(sqlText string, bqClinet *bigquery.Client) error {
+func ReadFromBq(sqlText string, bqClinet *bq.Client) error {
 	q := bqClinet.Query(sqlText)
 
 	ctx := context.Background()
@@ -68,7 +68,7 @@ func ReadFromBq(sqlText string, bqClinet *bigquery.Client) error {
 	}
 
 	for {
-		var values []bigquery.Value //Interface
+		var values []bq.Value //Interface
 		err := it.Next(&values)
 		if err == iterator.Done {
 			break
