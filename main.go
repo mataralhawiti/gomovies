68ec8069e43efcb89fa41f221a46ea62c58f8159
Matar7 <matar@linux.com>
2023-08-22 14:12:11 +0300

    add more bigquery operations

--- main.go
+++ main.go
@@ -26,6 +26,11 @@ func main() {
 	flag.StringVar(&PythonBinPath, "py", "python", "Your local machine python bin path. Default python")
 	flag.Parse()
 
+	// OS env
+	projectID := os.Getenv("GCP_PROJECT") // projectID := "golang-389808"
+	dataSet := os.Getenv("GCP_DATASET")
+	table := os.Getenv("GCP_TABLE")
+
 	switch runMod {
 	case 0:
 		// parse movies json file & display stats about your movies list
@@ -39,8 +44,6 @@ func main() {
 		// Read from BigQuery
 		sqltxt := userInput()
 
-		// projectID := "golang-389808"
-		projectID := os.Getenv("GCP_PROJECT")
 		if projectID == "" {
 			log.Fatal("- GCP_PROJECT environment variable must be set.\n# bash: export GCP_PROJECT=xxx\n# powershell: $env:GCP_PROJECT=xxx")
 		}
@@ -51,6 +54,26 @@ func main() {
 
 		// execute query
 		bigquery.ReadFromBq(sqltxt, c)
+	case 5:
+		// create dataset & table
+		if projectID == "" || dataSet == "" || table == "" {
+			log.Fatal("- GCP_PROJECT, GCP_DATASET, and GCP_TABLE environment variables must be set.\n# bash: export GCP_PROJECT=xxx\n# powershell: $env:GCP_PROJECT=xxx")
+		}
+
+		c := bigquery.CreateBqClient(projectID)
+		bigquery.CreateDataSet(dataSet, c)
+		bigquery.CreateTable(dataSet, table, c)
+
+	case 6:
+		// insert data
+		if projectID == "" || dataSet == "" || table == "" {
+			log.Fatal("- GCP_PROJECT, GCP_DATASET, and GCP_TABLE environment variables must be set.\n# bash: export GCP_PROJECT=xxx\n# powershell: $env:GCP_PROJECT=xxx")
+		}
+
+		c := bigquery.CreateBqClient(projectID)
+		myMovies := parser.ParseJSON(filePath)
+		bigquery.InsertIntoBq(dataSet, table, c, *myMovies)
+
 	default:
 		log.Fatal("invalid runMod")
 	}
