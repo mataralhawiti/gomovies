55a1a00839b373bfd3b73a40074b7eec3fab11e5
Matar7 <matar@linux.com>
2023-08-22 14:20:18 +0300

    move irrelevent funcs to internal pkg - helper.go

---
+++ internal/helper.go
@@ -0,0 +1,68 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mataralhawiti/gomovies/bigquery" // the directory Not the file!
+)
+
+// function to check if file exists
+func DoesFileExist(fileName string) {
+	_, error := os.Stat(fileName)
+
+	// check if error is "file not exists"
+	if os.IsNotExist(error) {
+		fmt.Printf("%v file does not exist\n", fileName)
+	} else {
+		fmt.Printf("%v file exist\n", fileName)
+	}
+}
+
+// check if value in Slice - Go 1.20
+// For Go 1.21, we should use package:slices
+func contains(slic []string, v string) (bool, error) {
+	for _, s := range slic {
+		if s == v {
+			return true, nil
+		}
+	}
+	return false, errors.New("Invalid query number")
+}
+
+func UserInput() string {
+	// BigQuery
+	ql := bigquery.QueriesList()
+	var ids []string
+
+	// get QueriesList ids
+	for _, k := range ql {
+		ids = append(ids, k["id"])
+	}
+
+	// Take user input
+	fmt.Println("Please enter query id .. ex: 2")
+	for _, items := range ql {
+		fmt.Printf("%v - %v\n", items["id"], items["desc"])
+	}
+
+	var qur string
+	fmt.Scan(&qur)
+
+	var sqltxt string
+
+	// check if user's input is valid
+	// if yes, extract SQL query text from predefined list
+	if ok, err := contains(ids, qur); ok {
+		for _, k := range ql {
+			if sqltxt = k["sqlText"]; k["id"] == qur {
+				return sqltxt
+			}
+		}
+	} else {
+		log.Fatal(err)
+	}
+	return sqltxt
+}
