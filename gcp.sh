887dfea0114b40905c36524f0cb9582f4917d1e3
Matar7 <matar@linux.com>
2023-08-29 15:05:36 +0300

    adding bigquery JSON schema

--- gcp.sh
+++ gcp.sh
@@ -11,3 +11,21 @@ export CDAP_ENDPOINT=$(gcloud beta data-fusion instances describe \
 export GCP_PROJECT=golang-389808
 export GCP_DATASET=gomovies
 export GCP_TABLE=movies
+export EMAIL=""
+# --
+# Cloud Functions
+## Create User-managed service account for Cloud Functions
+gcloud iam service-accounts create sa-matar-gcp \
+    --description="SA Matar GCP" \
+    --display-name="cloud functions SA"
+
+## Grant service account an IAM role on the project
+gcloud projects add-iam-policy-binding $GCP_PROJECT \
+    --member="serviceAccount:sa-matar-gcp@$GCP_PROJECT.iam.gserviceaccount.com" \
+    --role="roles/cloudfunctions.developer"
+
+## To allow users to impersonate the service account
+gcloud iam service-accounts add-iam-policy-binding \
+    sa-matar-gcp@$GCP_PROJECT.iam.gserviceaccount.com \
+    --member="user:$EMAIL" \
+    --role="roles/iam.serviceAccountUser"
